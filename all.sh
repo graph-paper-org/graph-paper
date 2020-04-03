1b540692e3e339bb05090e38ee6d1e4eae782aad
John Karahalis <john.karahalis@gmail.com>
2020-04-03 00:36:47 -0400

    Update all.sh to pass ShellCheck

--- all.sh
+++ all.sh
@@ -1,11 +1,11 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
 # adapted from https://github.com/hperrin/svelte-material-ui/blob/master/package-sh.sh
 
 for dir in packages/*; do
   if [ "$dir" != "packages/core" ]; then
-    cd "$dir"
-    $@
+    cd "$dir" || exit 1
+    "$@"
     cd ../..
   fi
 done
