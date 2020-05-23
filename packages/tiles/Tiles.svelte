82940f2060d5fcc022de503c5c2df5f0acbd42ba
William Lachance <wrlach@gmail.com>
2020-05-23 09:45:45 -0400

    Expose a bunch of grid properties in the Tiles component (#131)
    * Expose a bunch of grid properties in the Tiles component
    
    See https://github.com/graph-paper-org/graph-paper/pull/90#pullrequestreview-408649329
    
    * Left/right doesn't make sense for align content :)
    
    * Make sizes more deterministic

--- packages/tiles/Tiles.svelte
+++ packages/tiles/Tiles.svelte
@@ -2,10 +2,16 @@
   export let columns = 3;
   export let space = 1;
   export let columnSize = "auto";
+  export let justifyItems = "start";
+  export let justifyContent = "start";
+  export let alignItems = "normal";
+  export let alignContent = "start";
 </script>
 
 <div
   class="gp-tiles"
-  style="--columns: {columns}; --space: var(--space-{space}x); --column-size: {columnSize};">
+  style="--columns: {columns}; --space: var(--space-{space}x); --column-size: {columnSize};
+  --justify-items: {justifyItems}; --justify-content: {justifyContent};
+  --align-items: {alignItems}; --align-content: {alignContent};">
   <slot />
 </div>
--- packages/tiles/stories/TilesStory.svelte
+++ packages/tiles/stories/TilesStory.svelte
@@ -3,10 +3,20 @@
 
   export let columns = 3;
   export let space = 1;
+  export let justifyItems;
+  export let justifyContent;
+  export let alignItems;
+  export let alignContent;
 </script>
 
-<Tiles {columns} {space}>
+<Tiles
+  {columns}
+  {space}
+  {justifyItems}
+  {justifyContent}
+  {alignItems}
+  {alignContent}>
   {#each Array.from({ length: columns * 3 }).fill(null) as _, i}
-    <div class="_ _small" />
+    <div class="_ _small" style="height: {32 + (i % 2) * 32}px;" />
   {/each}
 </Tiles>
