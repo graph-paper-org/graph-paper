0cb695daddea193e79a048b43857373444ad0b4f
hamilton <hamilton.ulmer@gmail.com>
2020-06-01 14:12:29 -0700

    changes to MetricMouseover, Springable, Window1D

--- packages/elements/stories/Line/Window1D.svelte
+++ packages/elements/stories/Line/Window1D.svelte
@@ -4,6 +4,7 @@
   import { window1D } from "@graph-paper/core/utils/window-functions";
 
   export let data;
+  export let multi = false;
   export let key;
   export let defaultValue = "latest";
 
@@ -27,10 +28,19 @@
     if (w.current) return w.current;
     return 0;
   };
-
-  $: output = internalValue
-    ? get(data, internalValue, key, $x.domain())
-    : data[defaultValue === "latest" ? data.length - 1 : 0];
+  let output;
+  $: if (!multi) {
+    output = internalValue
+      ? get(data, internalValue, key, $x.domain())
+      : data[defaultValue === "latest" ? data.length - 1 : 0];
+  } else {
+    output = internalValue
+      ? data.map(([k, d]) => [k, get(d, internalValue, key, $x.domain())])
+      : data.map(([k, d]) => [
+          k,
+          d[defaultValue === "latest" ? d.length - 1 : 0],
+        ]);
+  }
 </script>
 
 <slot {output} />
